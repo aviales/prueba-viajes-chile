153040426bc1b85968174d6c8bfe4a2cfe0c35ef
Alonso Viales <avialesarrieta@gmail.com>
2021-01-25 08:45:28 -0600

    cambios 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+<script src="assets/js/bootstrap.min.js"></script>
 $(function(){
 
   $("a").click(function(event){
@@ -17,4 +19,27 @@ $(function(){
   $('[data-toggle="popover"]').popover();
 
 });
-<script src="assets/js/bootstrap.min.js"></script>
+
+
+
+
+// Call carousel manually
+$('#myCarouselCustom').carousel();
+
+// Go to the previous item
+$("#prevBtn").click(function(){
+    $("#myCarouselCustom").carousel("prev");
+});
+// Go to the previous item
+$("#nextBtn").click(function(){
+    $("#myCarouselCustom").carousel("next");
+});
+
+<script type="text/javascript">
+$('.carousel').carousel({
+     interval: 8000,
+     pause: true,
+     wrap: false
+    
+});
+</script>
