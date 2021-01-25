fe47250ec934acf357471b726bbb6228ce1204fe
Alonso Viales <avialesarrieta@gmail.com>
2021-01-25 11:48:41 -0600

    cambios 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 
 <script src="assets/js/bootstrap.min.js"></script>
 $(function(){
-
+  
   $("a").click(function(event){
     if (this.hash !== "") {
       event.preventDefault();
-
+      
       var hash = this.hash;
-
+      
       $("html, body").animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
@@ -15,31 +15,10 @@ $(function(){
       });
     }
   });
-
+  
   $('[data-toggle="popover"]').popover();
-
+  
 });
 
 
-
-
-// Call carousel manually
-$('#myCarouselCustom').carousel();
-
-// Go to the previous item
-$("#prevBtn").click(function(){
-    $("#myCarouselCustom").carousel("prev");
-});
-// Go to the previous item
-$("#nextBtn").click(function(){
-    $("#myCarouselCustom").carousel("next");
-});
-
-<script type="text/javascript">
-$('.carousel').carousel({
-     interval: 8000,
-     pause: true,
-     wrap: false
-    
-});
-</script>
+$('.carousel').carousel()
